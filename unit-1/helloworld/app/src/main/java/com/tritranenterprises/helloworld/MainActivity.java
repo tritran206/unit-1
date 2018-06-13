6a375c063a3d190ce53fc91ebe9befb038e848e4
Tri Tran <tritran20@gmail.com>
2018-06-13 12:19:17 -0700

    changed app icon

--- unit-1/helloworld/app/src/main/java/com/tritranenterprises/helloworld/MainActivity.java
+++ unit-1/helloworld/app/src/main/java/com/tritranenterprises/helloworld/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("MainActivity", "Hello World. An exception has been thrown.");
+
+        Log.e(LOG_TAG, "Hello World. An exception has been thrown.");
     }
 }
